40c5263d5437b95c0787fc39d3d50daa0bf30f34
pgordineer <62917911+pgordineer@users.noreply.github.com>
2025-03-26 16:23:27 -0400

    Updated

--- data/2_get_places.py
+++ data/2_get_places.py
@@ -18,10 +18,10 @@ def get_location(row):
         model="gpt-4o",
         messages=[
             {"role": "system", "content": "given a video description and title, give the location that the video was filmed.\
-              If it's not extremely clear, say \"no location found\" all lowercase with no punctuation. \
-             get as specific of a location as possible with the given inputs. \
+              If it's not extremely clear, really try your best to guess. Also, absolutely never place videos at the same location, if they are just name a location nearby so they are not duplicates. \
+             get as specific of a location as possible with the given inputs even if you have to guess. \
                 Especially if there is a bulilding named in the description, this should be the response.\
-             Ideally, output should be formatted 'location, city, state, country' if applicable. If not applicable, just skip"},
+             Output must be formatted 'location, city, state, country' if applicable. If not applicable, really try your best to get a location that can be geocoded"},
             {
                 "role": "user",
                 "content": f'title: "{row["title"]}", description:"{row["description"]}"'
