852a60554ce464b54b523e596bb4d286d0758c9c
pgordineer <62917911+pgordineer@users.noreply.github.com>
2025-04-18 21:06:59 -0400

    Update MapComponent.tsx

--- web/src/MapComponent.tsx
+++ web/src/MapComponent.tsx
@@ -1,94 +1,94 @@
 import { useEffect, useRef } from 'react';
 import L, { Marker } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-// https://willschenk.com/labnotes/2024/leaflet_markers_with_vite_build/
-import markerIconUrl from "leaflet/dist/images/marker-icon.png"
+import markerIconUrl from "leaflet/dist/images/marker-icon.png";
 import markerIconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadowUrl from "leaflet/dist/images/marker-shadow.png";
 import { VideoInfo } from './App';
 
-// https://willschenk.com/labnotes/2024/leaflet_markers_with_vite_build/
+// Configure Leaflet marker icons
 L.Icon.Default.prototype.options.iconUrl = markerIconUrl;
 L.Icon.Default.prototype.options.iconRetinaUrl = markerIconRetinaUrl;
 L.Icon.Default.prototype.options.shadowUrl = markerShadowUrl;
-L.Icon.Default.imagePath = ""
+L.Icon.Default.imagePath = "";
 
-const MapComponent = ({ data, activeVideo, setActiveVideo }: { data: VideoInfo[], activeVideo: string, setActiveVideo: (video: string) => void }) => {
-    //store a map of video ids to marker elements,
-    const markersRef = useRef<Map<string, Marker>>(new Map());
-    const mapRef = useRef<L.Map>(null);
-    const markerLayerRef = useRef<L.LayerGroup>(null)
-
+// Custom hook for initializing the map
+const useInitializeMap = (mapRef, markerLayerRef) => {
     useEffect(() => {
         const map = L.map('map').setView([51.1358, 1.3621], 5);
-        mapRef.current = map
+        mapRef.current = map;
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         }).addTo(map);
 
-        let markerLayer = L.layerGroup().addTo(map)
-        markerLayerRef.current = markerLayer
+        const markerLayer = L.layerGroup().addTo(map);
+        markerLayerRef.current = markerLayer;
 
         const coordsControl = new L.Control({ position: 'bottomleft' });
-
-        coordsControl.onAdd = (map: L.Map) => {
-            let ret = document.createElement("div")
+        coordsControl.onAdd = (map) => {
+            const ret = document.createElement("div");
             map.on("mousemove", (event) => {
-                ret.innerHTML = `<div class="control">${event.latlng.lat.toFixed(4)}, ${event.latlng.lng.toFixed(4)}</div>`
-            })
-            return ret
-        }
+                ret.innerHTML = `<div class="control">${event.latlng.lat.toFixed(4)}, ${event.latlng.lng.toFixed(4)}</div>`;
+            });
+            return ret;
+        };
 
-
-        const gitHubControl = new L.Control({position: 'bottomleft'})
-
+        const gitHubControl = new L.Control({ position: 'bottomleft' });
         gitHubControl.onAdd = () => {
-            let ret = document.createElement("div");
-            ret.innerHTML = "<a href=\"https://github.com/pgordineer/jetlagthegamethewebmap\"> GitHub </a>"
-            return ret
-        }
+            const ret = document.createElement("div");
+            ret.innerHTML = "<a href=\"https://github.com/pgordineer/jetlagthegamethewebmap\"> GitHub </a>";
+            return ret;
+        };
 
-        gitHubControl.addTo(map)
+        gitHubControl.addTo(map);
         coordsControl.addTo(map);
 
         return () => {
             map.remove();
         };
     }, []);
+};
+
+const MapComponent = ({ data, activeVideo, setActiveVideo }: { data: VideoInfo[], activeVideo: string, setActiveVideo: (video: string) => void }) => {
+    const markersRef = useRef<Map<string, Marker>>(new Map());
+    const mapRef = useRef<L.Map>(null);
+    const markerLayerRef = useRef<L.LayerGroup>(null);
+
+    useInitializeMap(mapRef, markerLayerRef);
 
     useEffect(() => {
-        let currentPopup = markersRef.current.get(activeVideo)
+        const currentPopup = markersRef.current.get(activeVideo);
         if (currentPopup) {
             currentPopup.openPopup();
-            mapRef.current?.panTo(currentPopup.getLatLng())
+            mapRef.current?.panTo(currentPopup.getLatLng());
         }
-    }, [activeVideo])
+    }, [activeVideo]);
 
     useEffect(() => {
         markerLayerRef.current?.clearLayers();
         markersRef.current = new Map<string, Marker>();
 
         data.forEach(element => {
-            if (element.geocode && element.geocode.length == 2) {
-                const coords = element.geocode
+            if (element.geocode && element.geocode.length === 2) {
+                const coords = element.geocode;
                 const marker = L.marker(coords)
                     .bindPopup(
-                        `<iframe class="video-player" src="https://www.youtube.com/embed/${element.videoId}" allowfullscreen></iframe>`
-                        , { maxWidth: undefined })
+                        `<iframe class="video-player" src="https://www.youtube.com/embed/${element.videoId}" allowfullscreen></iframe>`,
+                        { maxWidth: undefined }
+                    );
 
                 markerLayerRef.current?.addLayer(marker);
 
                 marker.on("click", () => {
-                    setActiveVideo(element.videoId)
-                })
+                    setActiveVideo(element.videoId);
+                });
 
-                markersRef.current.set(element.videoId, marker)
+                markersRef.current.set(element.videoId, marker);
             }
         });
-
-    }, [data])
+    }, [data]);
 
     return <div id="map"></div>;
 };
